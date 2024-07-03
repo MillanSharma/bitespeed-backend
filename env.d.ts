91f3dcaa875095e5b8ae28b6142a62d809b3c4d1
Millan Mac <rac@RACs-MacBook-Pro.local>
2024-07-03 18:52:27 +0530

    phase 2: scaffolding files inside /src and setting up validation for .env files

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+import type { Env } from '@/utils/env';
+ 
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends Env {}
+  }
+}
+ 
---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import process from 'node:process';
+import cors from 'cors';
+import express from 'express';
+import './utils/env';
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (_req, res) => {
+  res.json({
+    message: 'Welcome to the API!',
+  });
+});
+
+app.get('/status', (_req, res) => {
+  res.json({
+    message: 'Server is running',
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.info(`Server running at http://localhost:${PORT}`);
+});
